7eb69a9c9e153056bf5ab3078ca722aba072eb23
Min Xuan <hi@ngminxuan.com>
2021-10-09 15:01:59 +0800

    clicking one radio hides the other radios

--- index.js
+++ index.js
@@ -13,11 +13,35 @@ fetch(api_url)
           <img src="${radioData.image}" alt="radio image">
           <i class="fas fa-plus-circle"></i>
         </div>
-        <div class="radio" onClick="showDetails(event)">
+        <div class="radio">
           <p class="radio-name">${radioData.name}</p>
           <p class="radio-fm">${radioData.frequency}</p>
         </div>
         `)
+      });
 
+      // Hides all the images first, then display the selected one
+      const showDetails = (event) => {
+        const allPlayingDisp = document.querySelectorAll(".more-details");
+
+        allPlayingDisp.forEach((detail) => {
+          detail.classList.add("d-none");
+        });
+
+        const playingDisplay = event.currentTarget.previousElementSibling;
+        playingDisplay.classList.remove('d-none');
+
+        const currentlyPlayingDetails = document.querySelector('.currently-playing-details');
+        currentlyPlayingDetails.classList.remove('d-none');
+
+        const currentlyPlayingName = event.currentTarget.querySelector('.radio-name').innerText;
+        const playingRadioName = currentlyPlayingDetails.querySelector('.playing-radio-name');
+        playingRadioName.innerText = currentlyPlayingName;
+      };
+
+      // Makes all the elements with class 'radio' clickable
+      const radios = document.querySelectorAll('.radio');
+      radios.forEach((radio) => {
+        radio.addEventListener('click', showDetails);
+      });
     });
-  });
